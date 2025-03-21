583a648900e1a28f159c9657a16e390f4a661c80
Kenton Guarian <kguarian@ucsd.edu>
2025-03-21 13:06:32 -0700

    Update dualthresh.py
    cast variables to fix type error

--- neurodsp/burst/dualthresh.py
+++ neurodsp/burst/dualthresh.py
@@ -168,7 +168,9 @@ def _rmv_short_periods(sig, n_samples):
         osc_starts = np.insert(osc_starts, 0, 0)
     if osc_ends[-1] < osc_starts[-1]:
         osc_ends = np.append(osc_ends, len(osc_changes))
-
+    
+    osc_starts, osc_ends = np.array(osc_starts), np.array(osc_ends)
+    
     osc_length = osc_ends - osc_starts
     osc_starts_long = osc_starts[osc_length >= n_samples]
     osc_ends_long = osc_ends[osc_length >= n_samples]
